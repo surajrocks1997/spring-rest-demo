bb2cf3f0d6127b6bb35671b90f1c04347c6ba2cd
Suraj Gupta <surajrocks1997@gmail.com>
2022-08-17 17:38:57 +0530

    student not found exception and generic exception added

--- src/main/java/com/demo/springdemo/rest/StudentRestController.java
+++ src/main/java/com/demo/springdemo/rest/StudentRestController.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.springdemo.entity.Student;
+import com.demo.springdemo.entity.StudentErrorResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -35,9 +39,34 @@ public class StudentRestController {
 	public List<Student> getStudents() {
 		return theStudents;
 	}
-	
+
 	@GetMapping("/students/{studentId}")
 	public Student getStudent(@PathVariable int studentId) {
+
+		if (studentId >= theStudents.size() || studentId < 0) {
+			throw new StudentNotFoundException("Student Id not Found - " + studentId);
+		}
+
 		return theStudents.get(studentId);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+		StudentErrorResponse error = new StudentErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(e.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+
+		return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.BAD_REQUEST);
 	}
 }
